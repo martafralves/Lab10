b4e28d2d9528c0bbf2a34b97296188d3df2064b2
martafralves <74558958+martafralves@users.noreply.github.com>
2022-04-10 11:57:56 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function largest(){
+    var array = [1, 40, 56, 899, 38, 44];
+    var largestNum =0;
+    for (i=0; i<=largestNum; i++){
+        if(array[i]>largestNum){
+            var largestNum = array[i];
+        }
+    }
+    alert("The largest number is "+largestNum);
+}
